5b2b17f3d8dfd43e9a921bf2cd8c9412fde15d21
Jack San Vicente <RedcodingRoom@gmail.com>
2024-04-16 15:52:03 -0400

    Added Paused and unpaused menu conditions
    Game Manager will now pause the game or unpause the game when the proper conditions are called or when the escape key is pressed

--- Midterm Project/Assets/Scripts/GameManager.cs
+++ Midterm Project/Assets/Scripts/GameManager.cs
@@ -1,6 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
@@ -10,8 +13,7 @@ public class GameManager : MonoBehaviour
     [SerializeField] GameObject menuPause;
     [SerializeField] GameObject menuVictory;
     [SerializeField] GameObject menuGameOver;
-    public GameObject playerHPBer;
-
+    public GameObject playerHPBar;
     public GameObject player;
 
     public bool isPaused;
@@ -25,6 +27,53 @@ public class GameManager : MonoBehaviour
 
     void Update()
     {
-        
+        if(Input.GetButtonDown("Cancel") && menuActive == null)
+        {
+            statePaused();
+            menuActive = menuPause;
+            menuActive.SetActive(isPaused);
+        }
     }
+
+    public void statePaused()
+    {
+        isPaused = !isPaused;
+        Time.timeScale = 0;
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.Confined;
+    }
+
+    public void stateUnpaused()
+    {
+        isPaused = !isPaused;
+        Time.timeScale = 1;
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+        menuActive.SetActive(isPaused);
+        menuActive = null;
+    }
+
+    public void updateWinCondition(int updateVal)
+    {
+        //update the win condition and check if you have won
+        enemyCount -= updateVal;
+        
+        if(enemyCount <= 0 )
+        {
+            //win
+            statePaused();
+            menuActive = menuVictory;
+            menuActive.SetActive(isPaused);
+        }
+
+    }
+
+    public void gameOver()
+    {
+        statePaused();
+        menuActive = menuGameOver;
+        menuActive.SetActive(isPaused);
+    }
+
+
 }
